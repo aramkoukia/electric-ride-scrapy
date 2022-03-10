68ae70cfe37f1be9a8215febfc0c4cf2bfd047c8
aramkoukia <aramkoukia@gmail.com>
2022-03-09 19:47:55 -0800

    adding kia spider

---
+++ scrapcars/spiders/kia_com.py
@@ -0,0 +1,29 @@
+import scrapy
+from scrapy import Spider
+from scrapy.selector import Selector
+from scrapcars.items import ProductItem
+
+class KiaSpider(scrapy.Spider):
+    name = "kia_spider"
+
+    def start_requests(self):
+        urls= [
+            'https://www.kia.ca/'
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url= url, callback=self.parse)
+
+    # data-name="Aurora Cloud CBD" data-thc="~20" data-cbd="~550" data-category="Cannabis Oil" data-type="Hybrid"
+
+    def parse(self, response):
+        # product_list = response.css('div.product-tile__info > h4 *::text').extract()
+        product_list = response.css('#container')
+        for product in product_list:
+            item = ProductItem()
+            item['title'] = product.css('.product-tile__title::text').extract_first().strip()
+            item['vendor'] = product.css('.product-tile__vendor::text').extract_first().strip()
+            item['price'] = product.css('.product-tile__price::text').extract_first().strip()
+            #item['properties'] = product.css('.product-tile__properties::text').extract_first()
+            item['image'] = product.css('img::attr(src)').extract_first().strip()
+            yield item
